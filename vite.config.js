0cef5845029259f3071b7d5fc2925bce852d0f4a
johnkid363 <101831140+johnkid363@users.noreply.github.com>
2025-01-30 14:48:10 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/deploying-vite-project-french-conjugation/",
+  base: "/french-conjugation/",
   plugins: [vue()],
 });
